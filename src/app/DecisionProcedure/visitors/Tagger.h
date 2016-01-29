eb99ad5c929637b634d94f922eebcbfaf5d1fb7d
Tomas Fiedor <ifiedortom@fit.vutbr.cz>
2016-01-29 18:26:40 +0100

    Minor update.

--- src/app/DecisionProcedure/visitors/Tagger.h
+++ src/app/DecisionProcedure/visitors/Tagger.h
@@ -18,7 +18,13 @@ private:
 
     void _tagFormula(ASTForm*);
 public:
-    Tagger(TagList& tags) : VoidVisitor(Traverse::PreOrder), _tagList(tags), _tit(tags.begin()) {}
+    Tagger(TagList& tags) : VoidVisitor(Traverse::PreOrder), _tagList(tags), _tit(tags.begin()) {
+        std::cout << "[*] Loaded tags: {";
+        for(auto tag : _tagList) {
+            std::cout << tag << ", ";
+        }
+        std::cout << "\n";
+    };
 
     void visit(ASTForm_And*);
     void visit(ASTForm_Or*);
